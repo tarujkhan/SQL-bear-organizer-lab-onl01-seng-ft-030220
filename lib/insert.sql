bac991355372c21178a4bdb29c27277ca91da355
taroj khan <tarujkhan@yahoo.com>
2020-04-01 17:51:43 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,18 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 1, "Male", "Black", "Happy", 1);
+"INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "M", "Black", "Happy", 1);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "Male", "Brown", "Content", 1);
+"INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "Brown", "Content", 1);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 3, "Female", "Black", "Goofy", 1);
+"INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "Black", "Goofy", 1);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4, "Male", "Brown", "Okay", 1);
+"INSERT INTO bears VALUES (4, "Sergeant Brown", 4, "M", "Brown", "Okay", 1);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 5, "Female", "Black", "Happy", 1);
+"INSERT INTO bears VALUES (5, "Melissa", 5, "F", "Black", "Happy", 1);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 40, "Male", "Green", "Angry", 0);
+"INSERT INTO bears VALUES (6, "Grinch", 40, "M", "Green", "Angry", 0);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7, "Female", "Brown", "Happy", 0);
+"INSERT INTO bears VALUES (7, "Wendy", 7, "F", "Brown", "Happy", 0);"
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 10, "Male", "Black", "Angry", 1);
+"INSERT INTO bears VALUES (NULL, 10, "M", "Black", "Angry", 1);"
 
 -- INSERT INTO cats (name, age, breed) VALUES (NULL, NULL, "Tabby");
 -- name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, 

ac696f0076cc99be28a9f85983406855b3477f53
taroj khan <tarujkhan@yahoo.com>
2020-04-01 21:30:23 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,18 +1,18 @@
-"INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "M", "Black", "Happy", 1);"
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 1, "M", "Black", "Happy", 1);
 
-"INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "Brown", "Content", 1);"
+INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "Brown", "Content", 1);
 
-"INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "Black", "Goofy", 1);"
+INSERT INTO bears VALUES (3, "Tabitha", 3, "F", "Black", "Goofy", 1);
 
-"INSERT INTO bears VALUES (4, "Sergeant Brown", 4, "M", "Brown", "Okay", 1);"
+INSERT INTO bears VALUES (4, "Sergeant Brown", 4, "M", "Brown", "Okay", 1);
 
-"INSERT INTO bears VALUES (5, "Melissa", 5, "F", "Black", "Happy", 1);"
+INSERT INTO bears VALUES (5, "Melissa", 5, "F", "Black", "Happy", 1);
 
-"INSERT INTO bears VALUES (6, "Grinch", 40, "M", "Green", "Angry", 0);"
+INSERT INTO bears VALUES (6, "Grinch", 40, "M", "Green", "Angry", 0);
 
-"INSERT INTO bears VALUES (7, "Wendy", 7, "F", "Brown", "Happy", 0);"
+INSERT INTO bears VALUES (7, "Wendy", 7, "F", "Brown", "Happy", 0);
 
-"INSERT INTO bears VALUES (NULL, 10, "M", "Black", "Angry", 1);"
+INSERT INTO bears VALUES (8, NULL, 10, "M", "Black", "Angry", 1);
 
 -- INSERT INTO cats (name, age, breed) VALUES (NULL, NULL, "Tabby");
 -- name TEXT, age INTEGER, gender TEXT, color TEXT, temperament TEXT, 
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,27 +4,27 @@ def selects_all_female_bears_return_name_and_age
   end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name DESC;"
+  "SELECT name FROM bears ORDER BY name ASC;"
 
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 0 AND DESC;"
+  "SELECT name, age FROM bears WHERE alive=1 ORDER BY age ASC;"
 
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears WHERE ORDER BY age ASC LIMIT;"
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
  
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color FROM bears ORDER BY COUNT;"
+  "SELECT color FROM bears ORDER BY COUNT(*) LIMIT 1;"
 
 end
 
